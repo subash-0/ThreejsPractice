17ce64737e579f242b029b7309299f773b834385
subash-0 <subash67335@gmail.com>
2024-03-29 16:30:23 +0545

    working with cannon.js

---
+++ Cannonjs/main.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as CANNON from 'cannon-es';
+
+
+const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0,0,20);
+renderer.setSize(window.innerWidth, window.innerHeight);  
+document.body.appendChild(renderer.domElement);
+
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0,-9.81,0),
+
+});
+
+const timeStep = 1/60;
+
+function animate() {
+    world.step(timeStep)
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
