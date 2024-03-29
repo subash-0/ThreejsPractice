c321f962cf0ddf3f5d2d5bd3127bf384d248f24a
subash-0 <subash67335@gmail.com>
2024-03-29 15:27:03 +0545

    texture animation using sin and time

--- SHADDER/main.js
+++ SHADDER/main.js
@@ -11,7 +11,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
-// const orbit = new OrbitControls(camera, renderer.domElement);
+const orbit = new OrbitControls(camera, renderer.domElement);
 const uniforms={
 		
 	u_time :{type:'f', value:0.0},
@@ -40,7 +40,7 @@ const material = new THREE.ShaderMaterial( {
 const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
 camera.position.set(-1, -1, 20);
-// orbit.update();
+orbit.update();
 
 const clock = new THREE.Clock();
 
