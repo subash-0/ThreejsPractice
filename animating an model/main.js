277eb2276caa6bae083f9c7332215c7862676b91
subash-0 <subash67335@gmail.com>
2024-04-24 12:04:13 +0545

    cow model is added for animation

---
+++ animating an model/main.js
@@ -0,0 +1,46 @@
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/Addons.js"
+const Cow = new URL('./assest/Cow.gltf', import.meta.url)
+const renderer = new THREE.WebGLRenderer();
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement)
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    1000
+);
+
+camera.position.set(2,5,15);
+
+const scene = new THREE.Scene();
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.update()
+const ambidientLight= new THREE.AmbientLight(0xffffff, 1);
+const directionLight = new THREE.DirectionalLight(0xffffff,2);
+scene.add(ambidientLight)
+scene.add(directionLight)
+const gridHelper = new THREE.GridHelper(20,20);
+scene.add(gridHelper)
+const Loader = new GLTFLoader();
+
+Loader.load(Cow.href,(gltf)=>{
+    const model = gltf.scene;
+    scene.add(model)
+
+})
+
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+
+animate();
+
+window.addEventListener('resize',(e)=>{
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+renderer.render(scene,camera); 
+})
